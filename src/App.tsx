a6bd571b9f21c42017d6a9a525d5120c3d5c5529
Faiza203 <fiazfaiza@gmail.com>
2020-11-26 16:55:11 +0500

    Import Router from 'react-router'

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+
+import React, { Component } from 'react';
+import { Router} from 'react-router'
 import './App.css';
 import auth from 'firebase' 
-
 function App() {
 console.log(auth)
   return (
     <div className="App">
-    Welcome Tailor App
     </div>
   );
 }

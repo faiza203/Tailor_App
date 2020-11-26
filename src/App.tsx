d61b06f7a5cf57c9de0811b952cf788359f78a1a
Faiza203 <fiazfaiza@gmail.com>
2020-11-26 16:39:48 +0500

    Add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import auth from 'firebase' 
 
 function App() {
+console.log(auth)
   return (
     <div className="App">
     Welcome Tailor App

71196c918ccdfb4e961513fcdef7ba351b2aed26
Faiza203 <fiazfaiza@gmail.com>
2020-11-26 16:58:24 +0500

    Create home element

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,1 +1,8 @@
 import React from 'react';
+const Home = () => {
+    <div>
+       <h1>Welcome to Tailor App</h1> 
+       <button id="SignUpBtn">Sign Up</button>
+       <button id="SignInBtn">Sign In</button>
+    </div>
+}

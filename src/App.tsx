208ac91f8fd370bb64b92e840f9ffdc421d1128e
Faiza203 <fiazfaiza@gmail.com>
2020-11-27 09:55:26 +0500

    Import bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router';
 import { createBrowserHistory as createHistory } from 'history'
 import { Home, SignUp, SignIn, Nav } from './components/index';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const history = createHistory()

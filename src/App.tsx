a75995007aad84aa6f6977e737369f493ba1c7b1
Faiza203 <fiazfaiza@gmail.com>
2020-11-27 08:09:14 +0500

    Create SignUp Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Router, Route } from 'react-router';
 import { createBrowserHistory as createHistory } from 'history'
 import { Home } from './components/index';
-import auth from 'firebase';
 import './App.css';
 
 const history = createHistory()
 
 function App() {
-  console.log(auth);
   return (
     <div className="App">
       <Router history={history}>
---
+++ src/components/SignUp.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import auth from 'firebase';
+
+export class SignUp extends Component {
+    render() {
+        console.log(auth);
+        return (
+            <form>
+                <label>Email :</label>
+                <input type="email" placeholder="Please write email here" required />
+                <label>Password :</label>
+                <input type="password" placeholder="Enter password here" required />
+                <label>Confirm Password :</label>
+                <input type="password" placeholder="Rewrite password here" required />
+            </form>
+        )
+    }
+}

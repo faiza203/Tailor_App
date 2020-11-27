ab0cbd3313ea38f9cbc3907cbe33e1c3dc6bc1d9
Faiza203 <fiazfaiza@gmail.com>
2020-11-27 10:11:35 +0500

    Give style to Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,10 +3,25 @@ import { Link } from 'react-router-dom';
 export class Nav extends Component {
     render() {
         return (
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/SignUp">Sign Up</Link>
-                <Link to="/SignIn">Sign In</Link>
+            <div className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link to="/" className="nav-link">Home <span className="sr-only">(current)</span></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/SignUp" className="nav-link">Sign Up</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/SignIn" className="nav-link">Sign In</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }

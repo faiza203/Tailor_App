d98e4c3baf3e6ff0d79a67770ecca4e796be8ba1
Faiza203 <fiazfaiza@gmail.com>
2020-11-27 14:38:49 +0500

    Add img in home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 export class Home extends Component {
     render() {
         return (
-            <div>
+            <div className='homePage'>
                 <h1 className="h1 font-italic text-muted">Welcome to Tailor App</h1>
+                <div>
                 <Link className="btn btn-outline-dark" to="/SignUp">Sign Up</Link>
                 <Link className="btn btn-outline-dark" to="/SignIn">Sign In</Link>
-                <img src="https://thumbs.dreamstime.com/z/tailor-vector-logo-sewing-machine-logo-template-fashion-logo-sewing-machine-vector-icon-tailor-vector-logo-sewing-machine-logo-134228280.jpg" alt=""/>
+                </div>
+                <img height="350"  src="https://images-na.ssl-images-amazon.com/images/I/41aTFrD3D9L._SX425_.jpg" alt=""/>
             </div>
         )
     }

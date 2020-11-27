032811380db7c1f55e36e9342270c45e0d5d2056
Faiza203 <fiazfaiza@gmail.com>
2020-11-27 09:49:35 +0500

    Import Link from react-router-dom

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class Home extends Component {
     render() {
         return (
             <div>
                 <h1>Welcome to Tailor App</h1>
-                <button id="SignUpBtn">Sign Up</button>
-                <button id="SignInBtn">Sign In</button>
+                <Link to="/SignUp">Sign Up</Link>
+                <Link to="/SignIn">Sign In</Link>
             </div>
         )
     }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,10 @@
-import React , {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 export class Nav extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <div>
-            <Link to="/">Home</Link>
+                <Link to="/">Home</Link>
                 <Link to="/SignUp">Sign Up</Link>
                 <Link to="/SignIn">Sign In</Link>
             </div>

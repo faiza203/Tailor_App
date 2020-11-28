32c7228cffdea5056221463ea40518bdd50773a9
faiza203 <fiazfaiza981@gmail.com>
2020-11-28 16:23:47 +0500

    Add Dashboard in route

---
+++ src/components/DashBoard.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class DashBoard extends Component {
+    render() {
+        return (
+            <div >
+                <h1 className="h1 font-italic text-muted">DashBoard</h1>
+                 
+            </div>
+        )
+    }
+}

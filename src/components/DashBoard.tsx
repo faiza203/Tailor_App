7c4a597e192fe971af3bf05ebe4f81dd443e79e3
Faiza203 <fiazfaiza@gmail.com>
2020-11-30 12:23:20 +0500

    Add customers

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import { createBrowserHistory as createHistrory } from 'history'
+import { createBrowserHistory as createHistrory } from 'history';
+// import uuid from 'uuid';
+
 const history = createHistrory();
 
 
 const getFromFirebase = (tailor: any) => {
     const promise = firebase.firestore().collection('tailors').get().then(function (querySnapshot) {
         querySnapshot.forEach((doc: any) => {
-      console.log(doc);
+            console.log(doc);
         })
     });
     promise.then(() => {
@@ -17,6 +19,9 @@ const getFromFirebase = (tailor: any) => {
     })
 }
 
+const addCustomer = () =>{
+
+}
 export class DashBoard extends Component {
     render() {
         const promise = () => {
@@ -24,18 +29,32 @@ export class DashBoard extends Component {
                 const tailor = snap.val();
                 this.setState = tailor;
             });
-           return  getFromFirebase(this.setState);
+            return getFromFirebase(this.setState);
         }
         promise();
         const tailor = this.setState;
 
         return (
             <div>
-                <h1 className="h1 font-italic text-muted">
-                    {
-                        typeof (tailor) === typeof (() => { }) ? "loading" : tailor
-                    }
-                </h1>
+                {
+                    typeof (tailor) !== typeof (() => { }) ?
+                        <div>
+                            <h1 className="h1 font-italic text-muted">
+                                {tailor}
+                            </h1>
+                            <form onSubmit={addCustomer}>
+                                <input type="text" placeholder="Add Customer Name Here" />
+                                <button className="btn btn-outline-danger">Add customer
+                                    </button>
+                                    </form>
+                                    <div>
+                                        <h1 className="h1">Customers</h1>
+                                    </div>
+                        </div>
+                        : <h1 className="h1 font-italic text-muted">
+                            loading
+                        </h1>
+                }
             </div>
         )
     }

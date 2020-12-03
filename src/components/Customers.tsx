9a5be0797b85f60397a99c246f1ac2a6402cfbb0
Faiza203 <fiazfaiza@gmail.com>
2020-12-03 14:04:13 +0500

    Give styles to Customers

--- src/components/Customers.tsx
+++ src/components/Customers.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import firebase from 'firebase';
-// import { initialState } from "./reducer";
-
+import { history } from './index'
 const getClients = (tailor: string, customerState: any) => {
     const firestore = firebase.firestore();
     firestore
@@ -23,23 +22,23 @@ const getClients = (tailor: string, customerState: any) => {
 }
 
 export const Customers = (props: any) => {
-    const dispatch = useDispatch();
     const customerState = useSelector((state: any) => state);
 
     getClients(props.tailor, customerState);
-  
+
     return (
         <div>
-            {console.log(customerState)}
             <h2 className="h2 text-muted">Customers</h2>         {
                 customerState.clients.length !== 0 ?
                     customerState.clients.map((customer: any, index: number) => {
                         console.log(customer)
-                        return (<div key={index}><h1>{customer}</h1>
-                            <button id={customer + "measurment"}>Measurment</button></div>)
+                        return (<div key={index}><h3 className="h3 text-muted d-inline mt-2">{customer}</h3>
+                            <button id={customer + "measurment"} className="btn btn-outline-success d-inline" onClick={function () {
+                                history.push('/AddMeasurment')
+                            }} >Measurment</button>
+                        </div>)
                     }) :
-                    console.log("no User")
-
+                    null
             }
         </div>
     );

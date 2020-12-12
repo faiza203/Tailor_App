a1bc5b723520cadc4cb882ac14903bded850f589
faiza203 <fiazfaiza981@gmail.com>
2020-12-12 12:31:45 +0500

    Give style to already Measurment

--- src/components/AlreadyMeasurment.tsx
+++ src/components/AlreadyMeasurment.tsx
@@ -54,10 +54,9 @@ export function AlreadyMeasurment(props: any) {
                             console.log(event.target.value);
                         };
 
-
                         return (<div key={index} className="mr-5">
-                            <div  key={index} id="editM" className="d-none">   <div className="measurment">
-                                <form className="mr-5 " onSubmit={saveMeasurment}>
+                            <div key={index} id="editM" className="d-none">
+                                <form className="mr-5 editForm" onSubmit={saveMeasurment}>
                                     <input className="form-control" type="number" id="form1" placeholder="Length" value={measurment[1].Length} onChange={handleChange} required />
                                     <input className="form-control mt-1" type="number" placeholder="Width" value={measurment[1].Width} required />
                                     <input className="form-control mt-1" type="number" placeholder="Neck" value={measurment[1].Neck} required />
@@ -70,18 +69,20 @@ export function AlreadyMeasurment(props: any) {
                                     <button className="btn btn-outline-success d-inline" type="submit">Save Measurment</button>
                                     <button className="btn btn-outline-danger" type="button" onClick={() => { history.push("/DashBoard"); history.replace('/DashBoard') }}>Cancle</button>
                                 </form>
-                            </div></div>
-                            <li className="text-muted"> Length : {measurment[1].Length}</li>
-                            <li className="text-muted"> Width : {measurment[1].Width}</li>
-                            <li className="text-muted"> Neck : {measurment[1].Neck}</li>
-                            <li className="text-muted"> Waist : {measurment[1].Waist}</li>
-                            <li className="text-muted"> Bust : {measurment[1].Bust}</li>
-                            <li className="text-muted"> Chest : {measurment[1].Chest}</li>
-                            <li className="text-muted"> Arm Lenght : {measurment[1].ArmLenght}</li>
-                            <li className="text-muted"> Shoulder Length : {measurment[1].Shoulder}</li>
-                            <li className="text-muted"> Leg Lenght : {measurment[1].LegLenght}</li>
-                            <button className="btn btn-outline-danger" onClick={() => document.getElementById("editM")?.classList.remove("d-none")} >Edit</button>
-                            <button className="btn btn-outline-success">Cancle</button>
+                            </div>
+                            <ul className="measurment">
+                                <li className="text-muted"> Length : {measurment[1].Length}</li>
+                                <li className="text-muted"> Width : {measurment[1].Width}</li>
+                                <li className="text-muted"> Neck : {measurment[1].Neck}</li>
+                                <li className="text-muted"> Waist : {measurment[1].Waist}</li>
+                                <li className="text-muted"> Bust : {measurment[1].Bust}</li>
+                                <li className="text-muted"> Chest : {measurment[1].Chest}</li>
+                                <li className="text-muted"> Arm Lenght : {measurment[1].ArmLenght}</li>
+                                <li className="text-muted"> Shoulder Length : {measurment[1].Shoulder}</li>
+                                <li className="text-muted"> Leg Lenght : {measurment[1].LegLenght}</li>
+                                <button className="btn btn-outline-danger" onClick={() => document.getElementById("editM")?.classList.remove("d-none")} >Edit</button>
+                                <button className="btn btn-outline-success">Cancle</button>
+                            </ul>
                         </div>)
                     }
                 }) :
